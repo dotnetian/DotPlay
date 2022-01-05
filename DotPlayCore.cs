e98510a5d1c6358ce8b3b8a8ccea07e3b22addd5
Matin mohammadi <70327745+matinmn87@users.noreply.github.com>
2022-01-05 17:56:13 +0330

    Uploaded DotPlayCore

---
+++ DotPlayCore.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace Dotplay
+{
+	public class Scene
+	{
+		public static void Load (string scene)
+		{
+			SceneManager.LoadSceneAsync (scene);
+		}
+		public static void Load (int buildIndex)
+		{
+			SceneManager.LoadSceneAsync (buildIndex);
+		}
+		public static void Load (string scene, bool loadSceneAddetive)
+		{
+			if (loadSceneAddetive)
+				SceneManager.LoadSceneAsync (scene, LoadSceneMode.Additive);
+			else
+				SceneManager.LoadSceneAsync (scene);
+		}
+		public static void Load (int buildIndex, bool loadSceneAddetive)
+		{
+			if (loadSceneAddetive)
+				SceneManager.LoadSceneAsync (buildIndex, LoadSceneMode.Additive);
+			else
+				SceneManager.LoadSceneAsync (buildIndex);
+		}
+		public static void Unload ()
+		{
+			SceneManager.UnloadSceneAsync (SceneManager.GetActiveScene ().name);
+		}
+		public static void Unload (int buildIndex)
+		{
+			SceneManager.UnloadSceneAsync (buildIndex);
+		}
+		public static void Unload (string name)
+		{
+			SceneManager.UnloadSceneAsync (name);
+		}
+	}
+}
